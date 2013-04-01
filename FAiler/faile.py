dc846434094675c8c85afcc8de813156437ef3e2
tBunnyMan <WagThatTail@Me.com>
2013-03-31 20:48:34 -0700

    Clean up demo script and fix refactor errors in FAile

--- FAiler/faile.py
+++ FAiler/faile.py
@@ -10,7 +10,7 @@ class FAile():
     Represents a file downloaded from FurAffinity.
     The base parameters of this class are public access read safe by design
 
-    FAile.directory: the directory to the supplied file (or pwd if not supplied)
+    FAile.directory: the directory to the supplied file or pwd if not supplied
     FAile.filename: the full name of the supplied file. This never changes
     FAile.date: When this was uploaded
     FAile.artist: the name of the user who uploaded this
@@ -37,7 +37,7 @@ class FAile():
         :param faFile: Name of or path to a file from FA
         :raise: FAError if the file name cannot be parsed.
         """
-        self.directory = os.path.dirname(os.path.abspath(faFile))
+        self.directory = os.path.dirname(faFile)
         self.filename = os.path.basename(faFile)
         parsedName = re.match(self._FILE_RE, self.filename)
         if parsedName is None:
@@ -64,10 +64,11 @@ class FAile():
         >>> "{} - {}.{}".format(f2.artist, f2.name, f2.fileType)
         'shim - 1362116845.furball_shim_bday2013.jpg'
         >>> f2.clean_reupload()
-        >>> "{0.userName} - {0.submissionName}.{0.fileType}".format(f2)
+        >>> "{0.artist} - {0.name}.{0.fileType}".format(f2)
         'furball - shim_bday2013.jpg'
         """
-        check = re.match(self._FILE_RE, "{0.submissionName}.{0.fileType}".format(self))
+        check = re.match(self._FILE_RE,
+                         "{0.name}.{0.fileType}".format(self))
         if check is not None:
             (self.date, self.artist, self.name) = check.group(1, 2, 3)
-            self.date = datetime.datetime.fromtimestamp(self.date)
+            self.date = datetime.fromtimestamp(int(self.date))
--- bulkRename.py
+++ bulkRename.py
@@ -7,14 +7,20 @@ An example program for renaming lots of files with FAile
 Created by bunnyman on 2013/03/19.
 Copyright (c) 2013 Bunni.biz. All rights reserved.
 """
-import sys
 import os
+import argparse
 from FAiler import FAile, FAError
 
-RENAME = '{0.userName} - {0.submissionName}.{0.fileType}'
+RENAME = '{0.artist} - {0.name}.{0.fileType}'
+
+parser = argparse.ArgumentParser(description='Rename Files with FAile')
+parser.add_argument('file', nargs='+', help="Files to rename")
+parser.add_argument('--rename', default=RENAME, nargs=1,
+                    help='Optional format string to rename with')
+args = parser.parse_args()
 
 failes = []
-for x in sys.argv[1:]:
+for x in args.file:
     try:
         failes.append(FAile(x))
     except FAError as e:
@@ -24,8 +30,8 @@ for faile in failes:
     try:
         faile.clean_reupload()
         os.rename(
-            os.path.join(faile.directory, faile.basename),
-            os.path.join(faile.directory, RENAME.format(faile))
+            os.path.join(faile.directory, faile.filename),
+            os.path.join(faile.directory, args.rename.format(faile))
         )
     except OSError as e:
         print("File {} failed because of {}".format(faile.basename, e))
