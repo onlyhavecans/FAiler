a0988cf21b7543b28ec80012218dd38f84dde86a
tBunnyMan <WagThatTail@Me.com>
2013-03-31 17:26:19 -0700

    Turn off debugging

--- FAiler/faurl.py
+++ FAiler/faurl.py
@@ -194,9 +194,6 @@ class FAUrl():
             br.set_handle_referer(True)
             br.set_handle_equiv(True)
             br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-            br.set_debug_http(True)
-            br.set_debug_redirects(True)
-            br.set_debug_responses(True)
             if self._username is not None and self._password is not None:
                 loginPage = 'https://www.furaffinity.net/login'
                 br.open(loginPage)
